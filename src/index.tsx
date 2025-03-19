c8fcfc20b28850f0d65c3010b37d60e4cd515a6e
Filip Kowalewski <filip.kowalewski598@gmail.com>
2025-03-19 17:26:02 +0100

    install and setup react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/services";
 import { routes } from "@/pages";
 
 import "./index.css";
@@ -28,7 +30,9 @@ enableMocking().then(() => {
 
     createRoot(document.getElementById("root")!).render(
         <StrictMode>
-            <RouterProvider router={router} />
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
         </StrictMode>,
     );
 });
